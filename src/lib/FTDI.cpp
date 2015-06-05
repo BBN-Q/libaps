2a87d1e57670a1695478afd70c3ffdd927cf3702
Colm Ryan <cryan@bbn.com>
2015-06-05 10:21:36 -0400

    Add some logging info for what comes from FT_GetDeviceInfoList

--- src/lib/FTDI.cpp
+++ src/lib/FTDI.cpp
@@ -14,10 +14,10 @@ void FTDI::get_device_serials(vector<string> & deviceSerials) {
 	//Use the FTDI driver to get serial numbers (from "Simple" example)
 
 	FT_STATUS ftStatus;
-	FT_DEVICE_LIST_INFO_NODE *devInfo; 
+	FT_DEVICE_LIST_INFO_NODE *devInfo;
 	DWORD numDevices;
 
-	// create the device information list 
+	// create the device information list
 	ftStatus = FT_CreateDeviceInfoList(&numDevices);
 
 	if(!FT_SUCCESS(ftStatus)) {
@@ -27,29 +27,30 @@ void FTDI::get_device_serials(vector<string> & deviceSerials) {
 
 	FILE_LOG(logDEBUG) << "Found " << numDevices << " devices attached";
 
-	if (numDevices > 0) { 
-		// allocate storage for list based on numDevs 
-		devInfo = static_cast<FT_DEVICE_LIST_INFO_NODE*>( malloc(sizeof(FT_DEVICE_LIST_INFO_NODE)*numDevices) ); 
-		// get the device information list 
-		ftStatus = FT_GetDeviceInfoList(devInfo,&numDevices); 
+	if (numDevices > 0) {
+		// allocate storage for list based on numDevs
+		devInfo = static_cast<FT_DEVICE_LIST_INFO_NODE*>( malloc(sizeof(FT_DEVICE_LIST_INFO_NODE)*numDevices) );
+		// get the device information list
+		ftStatus = FT_GetDeviceInfoList(devInfo,&numDevices);
 
 		if (!FT_SUCCESS(ftStatus)) {
 			FILE_LOG(logERROR) << "Unable to run FT_GetDeviceInfoList with error code " << static_cast<int>(ftStatus);
 			return;
 		}
 
-		// available members in devInfo
-		// devInfo[i].Flags 
-		// devInfo[i].Type
-		// devInfo[i].ID
-		// devInfo[i].LocId
-		// devInfo[i].SerialNumber
-		// devInfo[i].Description
-		// devInfo[i].ftHandle); 
-
 		//Copy over the char buffers to the vector of strings
 		for(int devicect=0; (devicect < static_cast<int>(numDevices)) && (devicect<MAX_APS_DEVICES); devicect++){
+			FILE_LOG(logDEBUG2) << "Dev "<< devicect;
+			bool isOpen = devInfo[devicect].Flags & 0x1;
+			FILE_LOG(logDEBUG2) << "\tFlags = " << myhex << devInfo[devicect].Flags << (isOpen ? " (open)" : " (closed)");
+			FILE_LOG(logDEBUG2) << "\tType = " << myhex << devInfo[devicect].Type;
+			FILE_LOG(logDEBUG2) << "\tID = " << myhex << devInfo[devicect].ID;
+			FILE_LOG(logDEBUG2) << "\tLocId = " << myhex << devInfo[devicect].LocId;
+			FILE_LOG(logDEBUG2) << "\tSerialNumber = " << devInfo[devicect].SerialNumber;
+			FILE_LOG(logDEBUG2) << "\tDescription = " << devInfo[devicect].Description;
+			FILE_LOG(logDEBUG2) << "\tftHandle = " << devInfo[devicect].ftHandle;
 			deviceSerials.push_back(string(devInfo[devicect].SerialNumber));
+			FILE_LOG(logDEBUG) << "Added " << deviceSerials.back() << " to device list.";
 		}
 	}
 }
@@ -60,6 +61,7 @@ int FTDI::connect(const int & deviceID, FT_HANDLE & deviceHandle) {
 	ftStatus = FT_Open(deviceID, &deviceHandle);
 	if(!FT_SUCCESS(ftStatus)) {
 		FILE_LOG(logERROR) << "Unable to open connection to device " << deviceID;
+		FILE_LOG(logERROR) << "FT_STATUS is " << ftStatus;
 		return -1;
 	}
 	else{
@@ -97,17 +99,17 @@ int FTDI::isOpen(const int & deviceID) {
 	// test assumes that ftHandle is set to 0 is device is not open
 	// this assumption determined to be true via experimentation not documentation
 
-	FT_STATUS ftStatus; 
-	FT_HANDLE ftHandleTemp; 
-	DWORD numDevices; 
-	DWORD Flags; 
-	DWORD ID; 
-	DWORD Type; 
-	DWORD LocId; 
-	char SerialNumber[16]; 
+	FT_STATUS ftStatus;
+	FT_HANDLE ftHandleTemp;
+	DWORD numDevices;
+	DWORD Flags;
+	DWORD ID;
+	DWORD Type;
+	DWORD LocId;
+	char SerialNumber[16];
 	char Description[64];
 
-	// create the device information list 
+	// create the device information list
 	ftStatus = FT_CreateDeviceInfoList(&numDevices);
 
 	if(!FT_SUCCESS(ftStatus)) {
@@ -115,9 +117,9 @@ int FTDI::isOpen(const int & deviceID) {
 		return -1;
 	}
 
-	if ((numDevices > 0) && (deviceID < static_cast<int>(numDevices))) { 
+	if ((numDevices > 0) && (deviceID < static_cast<int>(numDevices))) {
 
-		ftStatus = FT_GetDeviceInfoDetail(deviceID, &Flags, &Type, &ID, &LocId, SerialNumber, Description, &ftHandleTemp);		
+		ftStatus = FT_GetDeviceInfoDetail(deviceID, &Flags, &Type, &ID, &LocId, SerialNumber, Description, &ftHandleTemp);
 		if (!FT_SUCCESS(ftStatus)) {
 			FILE_LOG(logERROR) << "Unable to run FT_GetDeviceInfoDetail with error code " << static_cast<int>(ftStatus);
 			return -2;
