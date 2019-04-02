63b5ee98c5ced0ed69fb64e72a4f6de0083b7b46
gribeill <guilhem.ribeill@gmail.com>
2019-04-02 16:49:05 -0400

    How much old logger stuff is there in here????

--- src/lib/FPGA.cpp
+++ src/lib/FPGA.cpp
@@ -755,7 +755,10 @@ int FPGA::set_bit(FT_HANDLE deviceHandle, const FPGASELECT & fpga, const int & a
 
 	FPGA::write_FPGA(deviceHandle, addr, currentState | mask, fpga);
 
-	if (FILELog::ReportingLevel() >= logDEBUG2) {
+  plog::Severity consoleSv = plog::get<CONSOLE_LOG>()->getMaxSeverity();
+  plog::Severity fileSv = plog::get<FILE_LOG>()->getMaxSeverity();
+
+	if ((consoleSv >= plog::debug) || (fileSv >= plog::debug)) {
 		// verify write
 		currentState = FPGA::check_cur_state(deviceHandle, fpga, addr);
 		if ((currentState & mask) == 0) {
